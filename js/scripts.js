f110538a8ba91d87d884c2cad2975279957a0c22
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-04-05 12:06:01 +0200

    beautify code

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,44 @@
-$(document).ready(function(){       
+$(document).ready(function () {
     //sticky navbar
-    var scroll_start = 0;  
+    var scroll_start = 0;
     var startchange = $('#startchange');
     var offset = startchange.offset();
-     if (startchange.length){
-    $(document).scroll(function() { 
-       scroll_start = $(this).scrollTop();
-       if(scroll_start > offset.top) {
-           $(".navbar-default").css('background-color', '#000000').css('opacity', '0.7');
-        } else {
-           $('.navbar-default').css('background-color', 'transparent').css('opacity', '1');
-        }
-    });
-     }
- });
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(".navbar-default").css('background-color', '#000000').css('opacity', '0.7');
+            } else {
+                $('.navbar-default').css('background-color', 'transparent').css('opacity', '1');
+            }
+        });
+    }
+});
 
- 
-$(".home").click(function() {
+
+$(".home").click(function () {
     $('html, body').animate({
         scrollTop: $("#home").offset().top
     }, 1000);
 });
 
-$(".about-us").click(function() {
+$(".about-us").click(function () {
     $('html, body').animate({
         scrollTop: $("#power").offset().top
     }, 1000);
 });
 
-$(".services").click(function() {
+$(".services").click(function () {
     $('html, body').animate({
         scrollTop: $("#services").offset().top
     }, 1000);
 });
-$(".prices").click(function() {
+$(".prices").click(function () {
     $('html, body').animate({
         scrollTop: $("#prices").offset().top
     }, 1000);
 });
-$(".contact").click(function() {
+$(".contact").click(function () {
     $('html, body').animate({
         scrollTop: $("#share").offset().top
     }, 1000);
